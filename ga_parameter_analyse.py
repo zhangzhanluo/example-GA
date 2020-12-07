f94e01690ac088c5063827803a8350d31f57945e
zhanluo <zhangzhanluo@sjtu.edu.cn>
2020-12-07 16:43:28 +0800

    增加了一些使出内容以查看具体的细节。

--- ga_parameter_analyse.py
+++ ga_parameter_analyse.py
@@ -1,3 +1,4 @@
+import time
 import numpy as np
 from matplotlib import pyplot as plt
 
@@ -34,19 +35,32 @@ plt.savefig(ga_solver.pic_path + 'Initial Population.png', dpi=300)
 plt.show()
 
 # 种群演化可视化 动画
+start_time = time.time()
 population_records = ga_solver.revolution()
-ga_animation = GAAnimation(pops=population_records)
+end_time = time.time()
+ga_animation = GAAnimation(pops=population_records, ga_solver=ga_solver)
 ga_animation.plot()
 
 # 种群演化可视化 箱线图
 ga_solver.plot_evolution(population_records, [0, 71], fig_size=(8, 3))
-plt.savefig(ga_solver.pic_path+'Revolution Boxplot.png', dpi=300)
+plt.savefig(ga_solver.pic_path + 'Revolution Boxplot.png', dpi=300)
 plt.tight_layout()
 plt.show()
 
 # 关键帧分析
-for n in [0, len(population_records)-1, 60, 25, 70]:
+for n in [0, len(population_records) - 1, 60, 25, 70]:
     ga_solver.plot_population(population_records[n], n)
-    plt.savefig(ga_solver.pic_path+'Generation {}.png'.format(n), dpi=300)
+    plt.savefig(ga_solver.pic_path + 'Generation {}.png'.format(n), dpi=300)
     plt.show()
 
+best_results = ga_solver.get_best_result(population_records[-1])
+print('********种群演化结果********\n')
+print('参数设定：')
+print('DNA_SIZE:{}'.format(ga_solver.dna_size))
+print('POPULATION SIZE:{}'.format(ga_solver.pop_size))
+print('CROSSOVER_RATE:{}'.format(ga_solver.crossover_rate))
+print('MUTATION_RATE:{}'.format(ga_solver.mutation_rate))
+print('N_GENERATION:{}'.format(ga_solver.n_generations))
+print('\n运行结果：')
+print('最佳解：{:.2f}\n最佳函数值:{:.2f}'.format(best_results[0], best_results[0]))
+print('运行时间：{:.2f}s'.format(end_time-start_time))
